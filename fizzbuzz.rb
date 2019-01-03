6bbd5010a0e3d20c810179e67b155eaeb648b689
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-03 21:05:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+    return "Fizzbuzz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    return "Fizzbuzz"
+  elsif int % 3 == 0
+    return "Fizz"
   else
-    return "nil"
+    return nil
   end
 end
